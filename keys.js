ceaabef23bb31da3d64e1e2ec39eb5a374505f0d
Ethan Franson <ethanfranson2@gmail.com>
2017-08-28 19:43:00 -0600

    file structure

---
+++ keys.js
@@ -0,0 +1,19 @@
+var twitter = require('twitter');
+var request = require('request');
+var spotify = require('spotify');
+
+
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
